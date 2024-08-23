94de825f1eaad82629a2ba7e26c9c14e0d12a034
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-08-23 10:30:47 +0530

    Time: 4 ms (84.86%) | Memory: 41.8 MB (61.01%) - LeetSync

--- 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
@@ -1,47 +1,48 @@
 class Solution {
     public String fractionAddition(String expression) {
-        int numerator = 0;
-        int denominator = 1;
-        int index = 0;
+        int numinator = 0;
+        int denominator =1;
+        int index =0;
         int length = expression.length();
-        
-        while (index < length) {
-            int sign = 1;
-            if (expression.charAt(index) == '-' || expression.charAt(index) == '+') {
-                sign = expression.charAt(index) == '-' ? -1 : 1;
-                index++;
+        while(index<length){
+            int sign =1;
+            if(expression.charAt(index)=='-' || expression.charAt(index)=='+'){
+            sign = expression.charAt(index)=='-'?-1:1;
+            index++;
             }
-
-            int numStart = index;
-            while (index < length && Character.isDigit(expression.charAt(index))) {
-                index++;
-            }
-            int num = Integer.parseInt(expression.substring(numStart, index)) * sign;
-            index++; // Skip '/'
-
-            int denomStart = index;
-            while (index < length && Character.isDigit(expression.charAt(index))) {
-                index++;
-            }
-            int denom = Integer.parseInt(expression.substring(denomStart, index));
-            
-            numerator = numerator * denom + num * denominator;
-            denominator *= denom;
-
-            int gcd = gcd(Math.abs(numerator), denominator);
-            numerator /= gcd;
-            denominator /= gcd;
+        int numst =index;
+        while(index<length && Character.isDigit(expression.charAt(index))){
+            index++;
         }
+        int num = Integer.parseInt(expression.substring(numst,index))*sign;
+        index++;
+
+        int demonst =index;
+        while(index<length &&  Character.isDigit(expression.charAt(index))){
+            index++;
+        }
+        int demon = Integer.parseInt(expression.substring(demonst,index));
+      
         
-        return numerator + "/" + denominator;
+        numinator = demon * numinator + num *denominator;
+        denominator *= demon;
+
+        int gcd = gcd(Math.abs(numinator) , denominator);
+        numinator /= gcd;
+        denominator /= gcd;
+
+        }
+        return numinator + "/" + denominator; 
+        
     }
 
-    public int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+    public int gcd(int a , int b) {
+        while(b!=0){
+            int temp =b;
+             b = a%b;
+             a = temp;
         }
         return a;
     }
+
 }
