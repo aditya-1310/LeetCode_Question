6ce0cb2db7889a2970a711b1d6d4356f6d0da605
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-08-29 10:15:29 +0530

    Time: 223 ms (5.09%) | Memory: 44.7 MB (74.30%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int removeStones(int[][] stones) {
+        // To track visited stones
+        Set<int[]> visited = new HashSet<>();
+        int numOfIslands = 0;
+
+        for (int[] stone : stones) {
+            if (!visited.contains(stone)) {
+                dfs(stones, stone, visited);
+                numOfIslands++;
+            }
+        }
+
+        // Total stones minus the number of islands gives the number of stones that can be removed
+        return stones.length - numOfIslands;
+    }
+
+    private void dfs(int[][] stones, int[] current, Set<int[]> visited) {
+        visited.add(current);
+
+        for (int[] stone : stones) {
+            if (!visited.contains(stone)) {
+                // Stones share the same row or column
+                if (stone[0] == current[0] || stone[1] == current[1]) {
+                    dfs(stones, stone, visited);
+                }
+            }
+        }
+    }
+}
