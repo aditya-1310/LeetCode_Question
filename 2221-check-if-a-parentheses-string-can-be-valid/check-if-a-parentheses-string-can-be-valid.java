d6e5b37f4c889416b16e5400b92769222e749d35
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-10-09 02:27:01 +0530

    Time: 16 ms (55.52%) | Memory: 45.6 MB (38.08%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -1,47 +1,37 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        // If the length of the string is odd, it's impossible to balance the parentheses
-        if (s.length() % 2 != 0) return false;
-
-        // First left-to-right pass to check for enough opening parentheses
-        int open = 0;  // Tracks fixed '('
-        int flexible = 0;  // Tracks positions that can be changed ('0' in locked)
-        
-        // Left to right: Ensure valid parentheses from the start
-        for (int i = 0; i < s.length(); i++) {
-            if (locked.charAt(i) == '1') {
-                if (s.charAt(i) == '(') {
-                    open++;  // Count fixed opening parentheses
-                } else {
-                    open--;  // We are encountering a fixed closing parenthesis
+        if(s.length()%2!=0) return false;
+        int ctn =0;int flexible=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(locked.charAt(i)=='1'){
+                if(ch=='('){
+                    ctn++;
+                }else{
+                    ctn--;
                 }
-            } else {
-                flexible++;  // We can change it to either '(' or ')'
             }
-
-            // At any point, if open < 0 and we can't recover using flexible, it's invalid
-            if (open < -flexible) return false;
-        }
-        
-        // Now, we check from the right to ensure valid closing parentheses
-        open = 0;
-        flexible = 0;
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (locked.charAt(i) == '1') {
-                if (s.charAt(i) == ')') {
-                    open++;  // Count fixed closing parentheses
-                } else {
-                    open--;  // We are encountering a fixed opening parenthesis
-                }
-            } else {
-                flexible++;  // We can change it to either '(' or ')'
+            else{
+                flexible++;
             }
-
-            // At any point, if open < -flexible and we can't recover using flexible, it's invalid
-            if (open < -flexible) return false;
+        if(ctn < -flexible) return false;
         }
 
-        return true;  // If we pass both checks, the string can be valid
+          ctn =0; flexible=0;
+         for(int i=s.length()-1;i>=0;i--){
+            char ch = s.charAt(i);
+            if(locked.charAt(i)=='1'){
+                if(ch==')'){
+                    ctn++;
+                }else{
+                    ctn--;
+                }
+            }
+            else{
+                flexible++;
+            }
+     if(ctn < -flexible) return false;
+        }
+        return true;
     }
 }
