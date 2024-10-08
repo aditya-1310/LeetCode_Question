09061cba2d01beaa9a433ca68d1b7718bb5d06ce
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-10-09 01:05:05 +0530

    Time: 40 ms (5.63%) | Memory: 45.3 MB (61.26%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,42 +1,40 @@
 class Solution {
     public int minInsertions(String s) {
-        int ans = 0;  // To count the number of insertions
-        int open = 0; // To track unmatched '('
+      Stack<Character> st = new Stack<>();
+      int insert =0;
+      int i=0;
+      while(i<s.length()){
+        char ch = s.charAt(i);
+        if(ch =='('){
+            st.push(ch);
+        }
+        else  if(ch ==')'){
+           
+            if(i+1<s.length() && s.charAt(i+1)==')'){
+                if(!st.isEmpty() && st.peek()=='(') {
+                    st.pop();
+                }
+                else insert++;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch == '(') {
-                // We have encountered an open parenthesis
-                open++;
-            } else {
-                // We have encountered a closing parenthesis
-                if (i + 1 < s.length() && s.charAt(i + 1) == ')') {
-                    // This is a pair of '))', consume one open if available
-                    if (open > 0) {
-                        open--;
-                    } else {
-                        // No matching open, so we need to add one '('
-                        ans++;
-                    }
-                    // Move past the next ')'
-                    i++;
-                } else {
-                    // This is a single ')', we need one more ')' to make it valid
-                    if (open > 0) {
-                        open--; // Use one open to match ')'
-                        ans++;  // Add one more ')' to make it '))'
-                    } else {
-                        // No matching open, so we need to add one '(' and one ')'
-                        ans += 2;
-                    }
+                i++;
+            }else{
+                if(!st.isEmpty() && st.peek()=='(') {
+                    st.pop();
+                    insert++;
+                }
+                else{
+                    insert+=2;
                 }
             }
+
         }
+        i++;
+      }
+      while(!st.isEmpty()){
+        st.pop();
+        insert+=2;
+      }
 
-        // If there are still unmatched open parentheses, we need to add two ')' for each
-        ans += open * 2;
-
-        return ans;
+      return insert;
     }
 }
