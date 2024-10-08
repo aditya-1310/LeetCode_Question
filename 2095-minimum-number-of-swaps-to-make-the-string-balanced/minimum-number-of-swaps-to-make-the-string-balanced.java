7ef28bfe2b2ce46aa0e35e9e15ee19f7db363cdc
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-10-08 11:46:11 +0530

    Time: 87 ms (28.88%) | Memory: 54.9 MB (60.00%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minSwaps(String s) {
+        Stack<Character> st = new Stack<>();
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+          char ch = s.charAt(i);
+          if(ch=='[') st.push(ch);
+          else{
+            if(!st.isEmpty()) st.pop();
+
+            else count ++;
+          }
+        }
+    return (count+1)/2;
+    }
+}
