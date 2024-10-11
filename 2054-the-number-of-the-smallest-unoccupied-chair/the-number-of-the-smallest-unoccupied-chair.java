41c7400a8c4d0b546e559e940fda80ff8d5cc85f
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-10-11 15:40:02 +0530

    Time: 49 ms (58.48%) | Memory: 52.6 MB (10.53%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,54 +1,38 @@
-import java.util.*;
-
 class Solution {
-    public int smallestChair(int[][] times, int targetFriend) {
+    public int smallestChair(int[][] times, int targetF) {
         int n = times.length;
-        
-        // Add index to times array to track which friend it belongs to
-        int[][] friends = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            friends[i][0] = times[i][0]; // arrival time
-            friends[i][1] = times[i][1]; // leaving time
-            friends[i][2] = i;           // friend index
-        }
-        
-        // Sort friends by arrival time
-        Arrays.sort(friends, (a, b) -> a[0] - b[0]);
-        
-        // Min-heap to track the smallest available chair
-        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
-        // Initially, we add as many chairs as we have friends
-        for (int i = 0; i < n; i++) {
-            availableChairs.offer(i);
-        }
+     int [][] friend = new int[n][3];
+     for(int i=0;i<n;i++){
+        friend[i][0] = times[i][0];
+        friend[i][1] = times[i][1];
+        friend[i][2] =i;
+     }
 
-        // Min-heap to track when a friend leaves {leaving time, chair number}
-        PriorityQueue<int[]> leavingQueue = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        
-        // Process each friend in order of arrival
-        for (int i = 0; i < n; i++) {
-            int arrival = friends[i][0];
-            int leaving = friends[i][1];
-            int friendIndex = friends[i][2];
-            
-            // Free chairs that are now available (based on the leaving time)
-            while (!leavingQueue.isEmpty() && leavingQueue.peek()[0] <= arrival) {
-                availableChairs.offer(leavingQueue.poll()[1]);
-            }
+     Arrays.sort(friend,(a,b)->a[0]-b[0]);
 
-            // Assign the smallest available chair
-            int assignedChair = availableChairs.poll();
+     PriorityQueue<Integer> avaliable = new PriorityQueue<>();
+     for(int i=0;i<n;i++){
+        avaliable.offer(i);
+     }
 
-            // If this is the target friend, return the assigned chair
-            if (friendIndex == targetFriend) {
-                return assignedChair;
-            }
+      PriorityQueue<int []> leavingtime = new PriorityQueue<>((a,b)->a[0]-b[0]);
 
-            // Add this friend's leaving time and their assigned chair to the leaving queue
-            leavingQueue.offer(new int[]{leaving, assignedChair});
-        }
+     for(int i=0;i<n;i++){
+        int arrival = friend[i][0];
+        int leaving = friend[i][1];
+        int friendindex = friend[i][2];
 
-        // Return -1 if the target friend is not found (edge case, though problem guarantees they will be)
-        return -1;
+        while(!leavingtime.isEmpty() && leavingtime.peek()[0]<=arrival){
+            avaliable.offer(leavingtime.poll()[1]);
+         }
+         int assignchair = avaliable.poll();
+
+         if(targetF == friendindex){
+            return assignchair;
+         }
+         leavingtime.offer( new int[]{leaving,assignchair});
+     }
+
+     return -1;
     }
 }
