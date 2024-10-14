fd84f6b8d7ba94a1952e57e30532a0fddb9159cf
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2024-10-15 01:26:52 +0530

    Time: 149 ms (63.19%) | Memory: 70.3 MB (79.12%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+         PriorityQueue<Long> maxHeap = new PriorityQueue<>();
+        
+        
+        int sum = 0;
+        // Calculate the total sum of the array and add elements to the heap.
+        for (int num : nums) {
+            
+            maxHeap.offer((long) num);
+        }
+        
+       
+        
+        // Continue reducing until we achieve at least half the reduction.
+        while (maxHeap.peek() < k) {
+            // Extract the largest element from the heap.
+            long largest = maxHeap.poll();
+            long val = largest*2+maxHeap.poll();
+            maxHeap.offer(val);
+            sum++; 
+        }
+        
+        return sum;
+    }
+}
