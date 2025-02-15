ceae50af7b6538d2c72380b4ba57b0fd880f4f8d
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2025-02-15 08:32:16 +0530

    Time: 10 ms (20.36%) | Memory: 44.2 MB (36.09%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,20 @@
 class Solution {
+    int dp[][] ;
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+         dp = new int[n][n];
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
+        }
+        return rec(dp,piles,0,n-1) > 0;
+    }
+    public int rec(int dp[][] , int [] piles ,int i , int j){
+        if(i>j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int takeleft = piles[i] -  rec(dp,piles,i+1,j);
+        int takeright = piles[j] - rec(dp,piles,i,j-1);
+
+        return dp[i][j] = Math.max(takeleft, takeright);
     }
 }
