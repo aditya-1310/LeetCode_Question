a03b0b4611464668e6f183ef324e178dad74c54f
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2025-02-15 23:51:53 +0530

    Time: 6 ms (62.31%) | Memory: 45.1 MB (12.31%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,64 +1,68 @@
-import java.util.*;
-
 class Solution {
-    class Node {
-        Node[] children;
-        boolean eow;
-
-        Node() {
-            children = new Node[26];
-            eow = false;
+    class  Node {
+    Node [] children;
+    boolean eow;
+     Node(){
+        children  = new Node[26];
+        for(int i=0;i<26;i++){
+            children[i] = null;
         }
+        eow = false;
     }
+}
+  Node root = new Node(); 
+  HashMap<String,Boolean> map = new HashMap<>();
 
-    Node root = new Node();
-    Map<String, Boolean> memo = new HashMap<>(); // Memoization Map
-
-    public void insert(String word) {
-        Node curr = root;
-        for (int i = 0; i < word.length(); i++) {
-            int idx = word.charAt(i) - 'a';
-            if (curr.children[idx] == null) {
+    public void insert(String word){
+            Node curr = root;
+        for(int i=0;i<word.length();i++){
+            int idx = word.charAt(i) -'a';
+            if(curr.children[idx]==null){
                 curr.children[idx] = new Node();
             }
-            curr = curr.children[idx];
-            if (i == word.length() - 1) {
-                curr.eow = true;
+            if(i == word.length()-1){
+                curr.children[idx].eow = true;
             }
+             curr = curr.children[idx];
         }
+    }
+    public boolean serach(String word){
+        Node curr = root;
+        for(int i=0;i<word.length();i++){
+            int index = word.charAt(i) -'a';
+            if(curr.children[index] == null) return false;
+
+            if(i==word.length()-1 && curr.children[index].eow == false) return false;
+            curr = curr.children[index];
+
+        }
+            return true;
     }
 
-    public boolean search(String word) {
-        Node curr = root;
-        for (int i = 0; i < word.length(); i++) {
-            int index = word.charAt(i) - 'a';
-            if (curr.children[index] == null) return false;
-            curr = curr.children[index];
-        }
-        return curr.eow;
-    }
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        for (String st : wordDict) {
-            insert(st);
-        }
-        return canBreak(s);
+            for(String st: wordDict){
+                insert(st);
+            }
+            return fin(s);
     }
-
-    public boolean canBreak(String word) {
-        if (word.length() == 0) {
+    public boolean fin(String word){
+        if(word.length() ==0){
             return true;
         }
-        if (memo.containsKey(word)) {
-            return memo.get(word);
+        if(map.containsKey(word)){
+            return map.get(word);
         }
-        for (int i = 1; i <= word.length(); i++) {
-            if (search(word.substring(0, i)) && canBreak(word.substring(i))) {
-                memo.put(word, true);
+        for(int i=1;i<=word.length();i++){
+            if(serach(word.substring(0,i)) && fin(word.substring(i))){
+                map.put(word,true);
                 return true;
             }
+
+
         }
-        memo.put(word, false);
-        return false;
+        map.put(word,false);
+            return false;
     }
+
 }
