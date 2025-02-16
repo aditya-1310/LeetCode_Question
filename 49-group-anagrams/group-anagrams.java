9bfaf92c47d898ec3f8c7f11164279c617ae2a3b
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2025-02-16 17:53:38 +0530

    Time: 9 ms (35.57%) | Memory: 54 MB (6.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,46 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-          HashMap<String, List<String>> map = new HashMap<>();
-        List<List<String>> result = new ArrayList<>();
-        for (int i = 0; i < strs.length; i++) {
-            char[] arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            String key = String.valueOf(arr);   
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-            map.get(key).add(strs[i]);
+    class Node{
+        List<String> data;
+        Node children[];
+        boolean eow ;
+        Node(){
+            children = new Node[26];
+            data = new ArrayList<>();
+            eow = false;
         }
-        for (List<String> list : map.values()) {
-            result.add(list);
-        }
-        return result;
     }
+        static Node root;
+        List<List<String>> ans;
+    public List<List<String>> groupAnagrams(String[] strs) {
+        root = new Node();
+        ans = new ArrayList<>();
+        for(int i=0;i<strs.length;i++){
+            Node temp= root;
+            String smm = strs[i];
+            char chs[] = smm.toCharArray();
+            Arrays.sort(chs);
+            for(char ch:chs){
+                Node child = temp.children[ch -'a'];
+                if(child == null) {
+                    temp.children[ch - 'a'] = new Node();
+                }
+                    temp  = temp.children[ch - 'a'];
+            }
+            temp.eow = true;
+            temp.data.add(smm);
+        }
+        dfs(root);
+
+        return ans;
+
+    }
+    public void dfs(Node root){
+        if(root.eow){
+            ans.add(root.data);
+        }
+        for(int i=0;i<26;i++){
+            if(root.children[i]!=null)
+            dfs(root.children[i]);
+        }
+    } 
 }
