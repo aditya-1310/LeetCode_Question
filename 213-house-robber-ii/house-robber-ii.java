1446ef9fbf239e0c13f0009c9c893c0cca317330
aditya-1310 <114504269+aditya-1310@users.noreply.github.com>
2025-04-17 23:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (91.02%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,28 +1,25 @@
 class Solution {
-    int t[] =new int[101];
+ 
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0]; 
+        
         int n = nums.length;
-        int[] farr = Arrays.copyOf(nums, nums.length - 1);
-        int[] larr = Arrays.copyOfRange(nums, 1, nums.length);
+         if (n == 1) return nums[0];
+         int dp[] = new int[n+1];
+         Arrays.fill(dp,-1);
+        int n1 = solve(nums,0,n-2,dp);
+        int dp1[] = new int[n+1];
+         Arrays.fill(dp1,-1);
+        int n2 = solve(nums,1,n-1,dp1);
 
-        Arrays.fill(t,-1);
-        int op1 = solve(farr,0,farr.length);
-        Arrays.fill(t,-1);
-        int op2 = solve(larr,0,larr.length);
-
-        return Math.max(op1,op2);
+        return Math.max(n1,n2);
+            
     }
-    public int solve(int nums[],int i,int n){
-        if(i>=n){
-            return 0;
-        }
-        if(t[i]!=-1) return t[i];
+    public int solve(int nums[],int i,int e,int dp[]){
+        if(i > e) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int t = nums[i] + solve(nums,i+2 ,e,dp);
+        int nt =  solve(nums,i+1,e,dp);
 
-        int take  = nums[i]+solve(nums,i+2,n);
-        int skip  = solve(nums,i+1,n);
-
-
-        return t[i] = Math.max(take,skip);
+            return dp[i] = Math.max(t,nt);
     }
 }
